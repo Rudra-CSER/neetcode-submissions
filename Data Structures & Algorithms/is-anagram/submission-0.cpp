db652c9a653cb165aaa0057ae45524423c4ca58d
Rudra Narayan Mondal <97387423+Rudra-CSER@users.noreply.github.com>
2026-04-19 12:36:00 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+     unordered_map<char , int > mpp;
+     for(int i = 0 ; i< s.length(); i++){
+        mpp[s[i]]++;
+     }
+     for(int i= 0 ; i< t.length() ; i++){
+        mpp[t[i]]--;
+     } 
+for (auto x : mpp) {
+            if (x.second != 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
